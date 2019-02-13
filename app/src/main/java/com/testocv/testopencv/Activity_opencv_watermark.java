4a388e6c60a2bc4d23459757855902076fcda59f
zhangshexin <zhangshexin@anne.com.cn>
2019-02-13 15:41:41 +0800

    可以简单的使用

--- app/src/main/java/com/testocv/testopencv/Activity_opencv_watermark.java
+++ app/src/main/java/com/testocv/testopencv/Activity_opencv_watermark.java
@@ -29,11 +29,13 @@ import java.util.List;
 public class Activity_opencv_watermark extends AppCompatActivity implements View.OnClickListener {
     private Bitmap originalBitmap1;
     private ActivityOpencvWatermarkBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        originalBitmap1 = BitmapUtil.readBitmap(this, R.drawable.people);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_opencv_watermark);
+        originalBitmap1 = BitmapUtil.readBitmap(this, R.drawable.comput_me1);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_opencv_watermark);
+        binding.imageView.setImageBitmap(originalBitmap1);
         initView();
     }
 
@@ -44,103 +46,113 @@ public class Activity_opencv_watermark extends AppCompatActivity implements View
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.button_generat:
                 //生成水印图
                 try {
-                    String mark=binding.editTextMarkcontent.getText().toString();
+                    String mark = binding.editTextMarkcontent.getText().toString();
                     generatMark(mark);
-                    Toast.makeText(this,"生成成功",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "生成成功", Toast.LENGTH_SHORT).show();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(this,"生成失败",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "生成失败", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.button_getmark:
                 //提出水印
                 try {
-                    if(tranMat!=null){
-                        getMark();
-//                        getWM();
-                        Toast.makeText(this,"成功",Toast.LENGTH_SHORT).show();
+                    if (tranMat != null) {
+//                        getMark();
+                        getWM();
+                        Toast.makeText(this, "成功", Toast.LENGTH_SHORT).show();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
-                    Toast.makeText(this,"失败",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "失败", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
     }
+
     private Mat tranMat;
+
     /**
-     * 生成水印
+     * 可用的生成水印
      */
-    private void generatMark(String mark){
-        Mat originalMat= new Mat();
+    private void generatMark(String mark) {
+        Mat originalMat = new Mat();
         Utils.bitmapToMat(originalBitmap1, originalMat);
 
-        Scalar scalar=new Scalar(0,0,255);
-        Point point=new Point(0,40);
-        tranMat=transformImageWithText(originalMat,mark,point,1D,scalar);
+        Scalar scalar = new Scalar(0, 0, 255);
+        Point point = new Point(20, 40);
+        tranMat = transformImageWithText(originalMat, mark, point, 1D, scalar);
 
 
 //        tranMat=ImgWatermarkUtil.addImageWatermarkWithText(originalMat,mark);
 
-        Utils.matToBitmap(tranMat,originalBitmap1);
+        Utils.matToBitmap(tranMat, originalBitmap1);
         binding.imageView.setImageBitmap(originalBitmap1);
+    }
+
+    private void generatMark2(String mark) {
+//        DFTUtil.getInstance().transformImageWithText();
     }
 
     /**
      * 提取水印
      */
-    private void getMark(){
+    private void getMark() {
 //        Mat mat=ImgWatermarkUtil.getImageWatermarkWithText(tranMat);
 
-        Mat invDft=new Mat();
-        Utils.bitmapToMat(originalBitmap1,invDft);
-        invDft= DFTUtil.getInstance().transformImage(invDft);
+        Mat invDft = new Mat();
+        Utils.bitmapToMat(originalBitmap1, invDft);
+        invDft = DFTUtil.getInstance().transformImage(invDft);
 //        Core.dft(tranMat,invDft);
 //        Mat restoredImage = new Mat();
 //        invDft.convertTo(restoredImage, CvType.CV_8U);
-        Utils.matToBitmap(invDft,originalBitmap1);
+        Utils.matToBitmap(invDft, originalBitmap1);
         binding.imageView.setImageBitmap(originalBitmap1);
     }
 
-    private String TAG=this.getClass().getName();
-private void getWM(){
-        Mat mat=new Mat();
-    Utils.bitmapToMat(originalBitmap1, mat);
+    private String TAG = this.getClass().getName();
+
+    /**
+     * 可用的提取水印方法
+     */
+    private void getWM() {
+        Mat mat = new Mat();
+        Utils.bitmapToMat(originalBitmap1, mat);
 
 
-    int p=mat.rows()*mat.cols();
-    Log.e(TAG, "改变前的大小: "+p );
+        int p = mat.rows() * mat.cols();
+        Log.e(TAG, "改变前的大小: " + p);
 
-    List<Mat> planes = new ArrayList<>();
-    List<Mat> allPlanes = new ArrayList<>();
-    Mat complexImage = new Mat();
-    // optimize the dimension of the loaded image
-    Mat padded = splitSrc(mat, allPlanes);
-    padded.convertTo(padded, CvType.CV_32F);
-    // prepare the image planes to obtain the complex image
-    planes.add(padded);
-    planes.add(Mat.zeros(padded.size(), CvType.CV_32F));
-    // prepare a complex image for performing the dft
-    Core.merge(planes, complexImage);
-    // dft
-    Core.dft(complexImage, complexImage);
-    Core.split(complexImage,planes);
-int after=complexImage.rows()*complexImage.cols();
-    Log.e(TAG, "改变后的大小: "+after );
-Mat mat1=new Mat();
-Mat mat2=new Mat();
-planes.get(0).convertTo(mat1,CvType.CV_8UC4);
-    planes.get(1).convertTo(mat2,CvType.CV_8UC4);
+        List<Mat> planes = new ArrayList<>();
+        List<Mat> allPlanes = new ArrayList<>();
+        Mat complexImage = new Mat();
+        // optimize the dimension of the loaded image
+        Mat padded = splitSrc(mat, allPlanes);
+        padded.convertTo(padded, CvType.CV_32F);
+        // prepare the image planes to obtain the complex image
+        planes.add(padded);
+        planes.add(Mat.zeros(padded.size(), CvType.CV_32F));
+        // prepare a complex image for performing the dft
+        Core.merge(planes, complexImage);
+        // dft
+        Core.dft(complexImage, complexImage);
+        Core.split(complexImage, planes);
+        int after = complexImage.rows() * complexImage.cols();
+        Log.e(TAG, "改变后的大小: " + after);
+        Mat mat1 = new Mat();
+        Mat mat2 = new Mat();
+        planes.get(0).convertTo(mat1, CvType.CV_8UC4);
+        planes.get(1).convertTo(mat2, CvType.CV_8UC4);
 
-    complexImage.convertTo(complexImage,CvType.CV_8U);
-    Bitmap newB=Bitmap.createBitmap(complexImage.width(),complexImage.height(), Bitmap.Config.ARGB_8888);
-    Utils.matToBitmap(mat2,newB);
-    binding.imageView.setImageBitmap(newB);
-}
+        complexImage.convertTo(complexImage, CvType.CV_8UC4);
+        Bitmap newB = Bitmap.createBitmap(complexImage.width(), complexImage.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mat1, newB);
+        binding.imageView.setImageBitmap(newB);
+    }
 
     private Mat transformImageWithText(Mat image, String watermarkText, Point point, Double fontSize, Scalar scalar) {
         List<Mat> planes = new ArrayList<>();
@@ -158,39 +170,39 @@ planes.get(0).convertTo(mat1,CvType.CV_8UC4);
         Core.dft(complexImage, complexImage);
         // 频谱图上添加文本
 
-        Imgproc.putText(complexImage, watermarkText, point, Core.FONT_HERSHEY_DUPLEX, fontSize, scalar,3,8,false);
+        Imgproc.putText(complexImage, watermarkText, point, Core.FONT_HERSHEY_DUPLEX, fontSize, scalar, 3, 8, false);
         Core.flip(complexImage, complexImage, -1);
-        Imgproc.putText(complexImage, watermarkText, point, Core.FONT_HERSHEY_DUPLEX, fontSize, scalar,3,8,false);
+        Imgproc.putText(complexImage, watermarkText, point, Core.FONT_HERSHEY_DUPLEX, fontSize, scalar, 3, 8, false);
         Core.flip(complexImage, complexImage, -1);
         return antitransformImage(complexImage, allPlanes);
     }
 
 
-    public Mat antitransformImage(Mat complexImg,List<Mat> allPlanes){
-        Mat invDft=new Mat();
-        Core.idft(complexImg,invDft, Core.DFT_SCALE|Core.DFT_REAL_OUTPUT,0);
-        Mat restoredImg=new Mat();
-        invDft.convertTo(restoredImg,CvType.CV_8U);
-        if(allPlanes.size()==0){
+    public Mat antitransformImage(Mat complexImg, List<Mat> allPlanes) {
+        Mat invDft = new Mat();
+        Core.idft(complexImg, invDft, Core.DFT_SCALE | Core.DFT_REAL_OUTPUT, 0);
+        Mat restoredImg = new Mat();
+        invDft.convertTo(restoredImg, CvType.CV_8U);
+        if (allPlanes.size() == 0) {
             allPlanes.add(restoredImg);
-        }else{
-            allPlanes.set(0,restoredImg);
+        } else {
+            allPlanes.set(0, restoredImg);
         }
-        Mat lastImg=new Mat();
-        Core.merge(allPlanes,lastImg);
+        Mat lastImg = new Mat();
+        Core.merge(allPlanes, lastImg);
         return lastImg;
     }
 
-    private Mat splitSrc(Mat img,List<Mat> plans){
-        if(plans==null){
-            plans=new ArrayList<>();
+    private Mat splitSrc(Mat img, List<Mat> plans) {
+        if (plans == null) {
+            plans = new ArrayList<>();
         }
-        Core.split(img,plans);
+        Core.split(img, plans);
         Mat padded;
-        if(plans.size()>0){
-            padded=plans.get(0);
-        }else
-            padded=img;
+        if (plans.size() > 0) {
+            padded = plans.get(0);
+        } else
+            padded = img;
         return padded;
     }
 
